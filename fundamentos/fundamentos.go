f513cac5dc7b2f0efc7258c298dd792fbd8e5aca
Fer_Carraro <carraro.fernando@gmail.com>
2020-04-19 16:18:20 -0500

    fundamentos.go editado

--- fundamentos/fundamentos.go
+++ fundamentos/fundamentos.go
@@ -16,6 +16,8 @@ func testA() {
 	var verdadero bool = 34 > 320
 	const TAM = 1000
 	numero_flotante := float64(123.8)
+	var nulo string = ""
+	var x, y, z bool // false false false
 	fmt.Printf("Dato: %d \n", dato)
 	fmt.Printf("Entero %d \n", entero)
 	fmt.Printf("Caracter %c \n", caracter)
@@ -24,4 +26,6 @@ func testA() {
 	fmt.Printf("Flotante %f \n", numero_flotante)
 	fmt.Printf("Booleano %t \n", verdadero)
 	fmt.Printf("Constante %d \n", TAM)
+	fmt.Println("Cadena ", nulo)
+	fmt.Println("Booleanos ", x, y, z)
 }
