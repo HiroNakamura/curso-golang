8e79fb7b5f0d840e1552b0a0fb4941edc1c229c7
Fer_Carraro <carraro.fernando@gmail.com>
2020-04-19 16:04:14 -0500

    Se edito archivo fundamentos.go

--- fundamentos/fundamentos.go
+++ fundamentos/fundamentos.go
@@ -9,20 +9,19 @@ func main() {
 func testA() {
 	fmt.Println("Fundamentos de programacion en Golang")
 	var dato byte = 1
-	entero := int("321")
+	entero := int(321.0)
 	caracter := '\u0000'
-	var real double = 90.32
-	var flotante float32 = 67.43f
+	var real float64 = 90.32
+	var flotante float32 = 67.43
 	var verdadero bool = 34 > 320
 	const TAM = 1000
-	numero_flotante = float32("123.8")
-	fmt.Printf("Dato: %d\n",dato)
-	fmt.Printf("Entero %d\n",entero)
-	fmt.Printf("Caracter %c\n",caracter)
-	fmt.Printf("Double %d\n",real)
-	fmt.Printf("Flotante %f\n",flotante)
-	fmt.Printf("Flotante %\n",numero_flotante)
-	fmt.Printf("Booleano %b\n",verdadero)
-	fmt.Printf("Constante %d\n",TAM)
+	numero_flotante := float64(123.8)
+	fmt.Printf("Dato: %d \n", dato)
+	fmt.Printf("Entero %d \n", entero)
+	fmt.Printf("Caracter %c \n", caracter)
+	fmt.Printf("Double %f \n", real)
+	fmt.Printf("Flotante %f \n", flotante)
+	fmt.Printf("Flotante %f \n", numero_flotante)
+	fmt.Printf("Booleano %t \n", verdadero)
+	fmt.Printf("Constante %d \n", TAM)
 }
-
